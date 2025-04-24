98330c108cc997adf358e3f599622016b4a0276f
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-04-24 23:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (36.86%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,19 @@
+def subset(nums:list,ans:list,temp:list,idx:int,d:dict,keys:list):
+    if (len(nums)==idx):
+        ans.append(temp[:]) 
+        return
+    if d[nums[idx]]==1:
+        subset(nums,ans,temp+[nums[idx]],idx+1,d,keys)
+    d[nums[idx]]=0
+    subset(nums,ans,temp,idx+1,d,keys)
+    d[nums[idx]]=1
+    
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        nums.sort()
+        d={n : 1 for n in set(nums)}
+        subset(nums,ans,[],0,d,list(d.keys()))
+        return ans
+
