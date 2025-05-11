850844cd03283bde1d270a0634f06ed34fa969f9
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-05-11 15:04:46 +0530

    Time: 4 ms (86.80%) | Memory: 18 MB (5.76%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -4,9 +4,7 @@ class Solution:
             return True
         if n<3:
             return False
-        while n!=0:
-            if n==1:
-                return True
+        while n!=0 and n!=1:
             if n%3!=0:
                 return False
             n=n//3
