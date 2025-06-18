e910d037f518f46b1d5c8eaf7dfc739dbd69e15d
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-06-18 17:58:45 +0530

    Time: 246 ms (31.12%) | Memory: 49.7 MB (10.08%) - LeetSync

---
+++ 403-frog-jump/frog-jump.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        if stones[1] - stones[0] != 1:
+            return False
+        dp = [[False] * len(stones) for _ in range(len(stones))]
+        return self.find(stones,0,1,dp)
+    def find(self,stones:list,lastindex:int, currentindex:int,dp:list[list]):
+        if currentindex==len(stones)-1:
+            return True
+        if dp[lastindex][currentindex]:
+            return False
+        lastjump=stones[currentindex]-stones[lastindex]
+        nextindex=currentindex+1
+        while nextindex<len(stones) and stones[nextindex]<=stones[currentindex]+lastjump+1:
+            newjump=stones[nextindex]-stones[currentindex]
+            jump=newjump-lastjump
+            if (jump>=-1 and jump<=1):
+                if self.find(stones,currentindex,nextindex,dp):
+                    return True
+            nextindex+=1
+        dp[lastindex][currentindex]=True
+        return False
