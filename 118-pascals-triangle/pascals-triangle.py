63675690c5e51cde1e698e2df7d2a8ff439116b6
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-07-19 08:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.13%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -5,12 +5,6 @@ class Solution:
         self.find(dp,n, 0,0)
         return dp
     def find(self,dp:List[List[int]],n:int,i:int,j:int):
-        # for i in range(1,n+1):
-        #     for j in range(1,n+1):
-        #         if i==j:
-        #             dp[i][j]=1
-        #         else:
-        #             dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
         dp.append([1])
         for i in range(1,n):
             array=[]
