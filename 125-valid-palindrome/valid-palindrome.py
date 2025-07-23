655aa5e9118da5ea73f6b5048f74f526514e0aec
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-07-24 00:37:45 +0530

    Time: 11 ms (86.65%) | Memory: 14.1 MB (28.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,18 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        s=s.lower()
+        s=re.sub(r'[^a-z0-9]','',s)
         start=0
         end=len(s)-1
         while (start<end):
-            if s[start].isalnum() and s[end].isalnum():
-                if s[start].lower()==s[end].lower():
-                    start+=1
-                    end-=1
-                else:
-                    return False
-            elif s[start].isalnum() ==False:
+            if s[start]==s[end]:
                 start+=1
-            elif s[end].isalnum() ==False:
                 end-=1
+            else:
+                return False
         return True
-            
