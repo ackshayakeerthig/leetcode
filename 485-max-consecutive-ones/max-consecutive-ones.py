28a36451e5c6e2595c770eb1923ecd312a478d00
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-07-24 19:46:11 +0530

    Time: 39 ms (29.32%) | Memory: 13.5 MB (89.07%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -4,13 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max=0
+        maxx=0
         count=0
-        for i in range(len(nums)):
-            if nums[i]==1:
+        for num in nums:
+            if num==1:
                 count+=1
-                if count>max:
-                    max=count
+                maxx=max(maxx,count)
             else:
                 count=0
-        return max
+        return maxx
