3ec0738d6adb552ff03a447145223d2091d476cf
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-07-24 22:36:24 +0530

    Time: 4 ms (89.70%) | Memory: 13 MB (63.32%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findCircleNum(self, graph):
+        """
+        :type isConnected: List[List[int]]
+        :rtype: int
+        """
+        visited=[0]*len(graph)
+        vertices=len(graph)
+        count=0
+        for i in range(vertices):
+            if visited[i]==0:
+                count+=1
+                self.dfs(i,visited,graph)
+        return count
+    def dfs(self,i,visited,graph):
+        visited[i]=1
+        for j in range(len(graph)):
+            if visited[j]==0 and graph[i][j]:
+                self.dfs(j,visited,graph)
+    
+
+        
