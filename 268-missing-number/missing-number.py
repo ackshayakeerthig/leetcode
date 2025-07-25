ac7480d990d6de26bd6f5a608ec874637557dc14
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-07-25 22:33:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (88.92%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -16,9 +16,11 @@ class Solution(object):
         #     j+=1
         # x=x^n
         # return x^y
+        # n=len(nums)
+        # necessarysum=n*(n+1)//2
+        # actualsum=0
+        # for i,val in enumerate(nums):
+        #     actualsum+=val
+        # return necessarysum-actualsum
         n=len(nums)
-        necessarysum=n*(n+1)//2
-        actualsum=0
-        for i,val in enumerate(nums):
-            actualsum+=val
-        return necessarysum-actualsum
+        return n*(n+1)//2-sum(nums)
