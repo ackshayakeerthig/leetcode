54749b10f3452ba8c7ef6bf1cb54ea180e202912
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-07-25 22:32:10 +0530

    Time: 5 ms (47.15%) | Memory: 13.5 MB (30.17%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -4,16 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        i=0
+        # i=0
+        # n=len(nums)
+        # j=0
+        # x=0
+        # y=0
+        # while  j<n:
+        #     x=x^i
+        #     i+=1
+        #     y=y^nums[j]
+        #     j+=1
+        # x=x^n
+        # return x^y
         n=len(nums)
-        j=0
-        x=0
-        y=0
-        while  j<n:
-            x=x^i
-            i+=1
-            y=y^nums[j]
-            j+=1
-        x=x^n
-        return x^y
-        
+        necessarysum=n*(n+1)//2
+        actualsum=0
+        for i,val in enumerate(nums):
+            actualsum+=val
+        return necessarysum-actualsum
