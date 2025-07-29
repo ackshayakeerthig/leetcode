2ce3aae983e5b8297ee62896e83495414f9e716b
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-07-29 23:52:16 +0530

    Time: 110 ms (86.91%) | Memory: 21.5 MB (24.60%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -3,24 +3,23 @@ class Solution:
         rows=len(mat)
         cols=len(mat[0])
         dist=[[float('inf')]*cols for _ in range(rows)]
-        # visited=[[0]*cols for _ in range(rows)]
+        visited=[[0]*cols for _ in range(rows)]
         q=deque()
         for row in range(rows):
             for col in range(cols):
-                # if mat[row][col]==0 and not visited[row][col]:
                 if mat[row][col]==0 :
                     q.append((row,col,0))
-                    # visited[row][col]=1
+                    visited[row][col]=1
                     dist[row][col]=0
         adj=[(0,1),(1,0),(0,-1),(-1,0)]
         while (len(q)>0):
             currow,curcol,step=q.popleft()
             for drow,dcol in adj:
                 adjrow,adjcol=drow+currow,dcol+curcol
-                # if 0<=adjrow<rows and 0<=adjcol<cols and not visited[adjrow][adjcol] and mat[adjrow][adjcol]==1:
-                if 0<=adjrow<rows and 0<=adjcol<cols and  mat[adjrow][adjcol]==1 and dist[adjrow][adjcol]>step+1:
+                if 0<=adjrow<rows and 0<=adjcol<cols and not visited[adjrow][adjcol] and mat[adjrow][adjcol]==1:
+                # if 0<=adjrow<rows and 0<=adjcol<cols and  mat[adjrow][adjcol]==1 and dist[adjrow][adjcol]>step+1:
                     q.append((adjrow,adjcol,step+1))
                     dist[adjrow][adjcol]=step+1
-                    # visited[adjrow][adjcol]=1
+                    visited[adjrow][adjcol]=1
             
         return dist
