1b2f8358016083ec1a183a2a5fc8c6614ba0f254
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-07-30 21:51:29 +0530

    Time: 4 ms (41.89%) | Memory: 19.1 MB (10.34%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d={}
-        for i in range(len(nums)):
-            needed=target-nums[i]
-            if needed in d:
-                return [d[needed],i]
-            d[nums[i]]=i
-        return -1
+        # d={}
+        # for i in range(len(nums)):
+        #     needed=target-nums[i]
+        #     if needed in d:
+        #         return [d[needed],i]
+        #     d[nums[i]]=i
+        # return -1
+        d = {}
+        append = d.__setitem__  # Faster than d[num] = i
+        get = d.get              # Faster than "in d"
+        
+        for i, num in enumerate(nums):
+            match = get(target - num)
+            if match is not None:
+                return [match, i]
+            append(num, i)
+        return []
