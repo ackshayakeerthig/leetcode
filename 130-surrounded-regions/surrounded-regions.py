1065df7ca9e43ce8643d2acf8a46e1f0c4ff88fb
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-03 23:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (88.13%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,37 +1,30 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        rows=len(board)
-        cols=len(board[0])
-        visited=[[0]*cols for _ in range(rows)]
-        for i in range(cols):
-            if board[0][i] == 'O':
-                self.bfs(board, visited, 0, i)
-            if board[rows - 1][i] == 'O':
-                self.bfs(board, visited, rows - 1, i)
-        
+        if not board:
+            return
+
+        rows, cols = len(board), len(board[0])
+
+        def dfs(r, c):
+            if 0 <= r < rows and 0 <= c < cols and board[r][c] == 'O':
+                board[r][c] = '#'
+                dfs(r + 1, c)
+                dfs(r - 1, c)
+                dfs(r, c + 1)
+                dfs(r, c - 1)
+
+        # 1. Start DFS from boundary 'O's
         for i in range(rows):
-            if board[i][0] == 'O':
-                self.bfs(board, visited, i, 0)
-            if board[i][cols - 1] == 'O':
-                self.bfs(board, visited, i, cols - 1)
+            dfs(i, 0)
+            dfs(i, cols - 1)
+        for j in range(cols):
+            dfs(0, j)
+            dfs(rows - 1, j)
+
+        # 2. Flip non-visited 'O's to 'X', and '#' back to 'O'
         for i in range(rows):
             for j in range(cols):
-                if not visited[i][j]:
-                    board[i][j]='X'
-    def bfs(self,board,visited,row,col):
-        q=deque()
-        q.append((row,col))
-        visited[row][col]=1
-        adjacent=[(1,0),(0,1),(-1,0),(0,-1)]
-        while(len(q)>0):
-            currow,curcol=q.popleft()
-            for drow,dcol in adjacent:
-                adjrow,adjcol=drow+currow,dcol+curcol
-                if 0<=adjrow<len(board) and 0<=adjcol<len(board[0]) and board[adjrow][adjcol]=='O' and not visited[adjrow][adjcol]:
-                    visited[adjrow][adjcol]=1
-                    q.append((adjrow,adjcol))
-
-        
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == '#':
+                    board[i][j] = 'O'
