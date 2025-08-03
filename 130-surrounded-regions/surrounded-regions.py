ecc086cdcf70fe2112cfb6f21d0806a9a9c382ce
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-04 00:07:08 +0530

    Time: 5 ms (65.88%) | Memory: 21.6 MB (88.13%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -2,29 +2,29 @@ class Solution:
     def solve(self, board: List[List[str]]) -> None:
         if not board:
             return
-
-        rows, cols = len(board), len(board[0])
-
-        def dfs(r, c):
-            if 0 <= r < rows and 0 <= c < cols and board[r][c] == 'O':
-                board[r][c] = '#'
-                dfs(r + 1, c)
-                dfs(r - 1, c)
-                dfs(r, c + 1)
-                dfs(r, c - 1)
-
-        # 1. Start DFS from boundary 'O's
+        rows,cols=len(board),len(board[0])
+        adjacent=[()]
+        def dfs(row,col):
+            if 0<=row<rows and 0<=col<cols and board[row][col]=='O':
+                board[row][col]='#'
+                dfs(row+1,col)
+                dfs(row,col+1)
+                dfs(row-1,col)
+                dfs(row,col-1)
         for i in range(rows):
-            dfs(i, 0)
-            dfs(i, cols - 1)
-        for j in range(cols):
-            dfs(0, j)
-            dfs(rows - 1, j)
-
-        # 2. Flip non-visited 'O's to 'X', and '#' back to 'O'
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j] == 'O':
-                    board[i][j] = 'X'
-                elif board[i][j] == '#':
-                    board[i][j] = 'O'
+            if board[i][0]=='O':
+                dfs(i,0)
+            if board[i][cols-1]=='O':
+                dfs(i,cols-1)
+        for i in range(cols):
+            if board[0][i]=='O':
+                dfs(0,i)
+            if board[rows-1][i]=='O':
+                dfs(rows-1,i)
+        
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col]=='#':
+                    board[row][col]='O'
+                elif board[row][col]=='O':
+                    board[row][col]='X'
