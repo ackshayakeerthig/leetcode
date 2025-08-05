37107ed6acceb69f72a79bfa48e1a300dafbb37f
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-05 23:42:09 +0530

    Time: 409 ms (31.64%) | Memory: 18.1 MB (15.44%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,31 +1,32 @@
-from typing import List
-
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
+        n=len(nums)
         nums.sort()
-
-        ans = []
-        for i in range(n - 3):
-            if i > 0 and nums[i] == nums[i - 1]:
+        ans=[]
+        for i in range(n-3):
+            if i>0 and nums[i]==nums[i-1]:
                 continue
-            for j in range(i + 1, n - 2):
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            for j in range (i+1,n-2):
+                if j>i+1 and nums[j]==nums[j-1]:
                     continue
-                k, l = j + 1, n - 1
-                while k < l:
-                    summ = nums[i] + nums[j] + nums[k] + nums[l]
-                    if summ == target:
-                        ans.append([nums[i], nums[j], nums[k], nums[l]])
-                        k += 1
-                        l -= 1
-                        # skip duplicates after storing result
-                        while k < l and nums[k] == nums[k - 1]:
-                            k += 1
-                        while k < l and nums[l] == nums[l + 1]:
-                            l -= 1
-                    elif summ < target:
-                        k += 1  # no duplicate skipping here!
+                k=j+1
+                l=n-1
+                while k<l:
+                    summ=nums[i]+nums[j]+nums[k]+nums[l]
+                    if summ==target:
+                        ans.append([nums[i],nums[j],nums[k],nums[l]])
+                        k+=1
+                        l-=1
+                        while k<l and nums[k]==nums[k-1]:
+                            k+=1
+                        while k<l and nums[l]==nums[l+1] :
+                            l-=1
+                    elif summ<target:
+                        k+=1
                     else:
-                        l -= 1  # no duplicate skipping here!
+                        l-=1
         return ans
+
+
+
+        
