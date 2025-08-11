707bcb18cd23aa44a292c1806db662446b775f1b
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-11 15:37:10 +0530

    Time: 3 ms (76.16%) | Memory: 18.9 MB (85.70%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -7,11 +7,13 @@ class Solution:
             indegree[course]+=1
         q=deque([i for i in range(n) if indegree[i]==0])
         order=[]
+        taken=0
         while q:
             course=q.popleft()
+            taken+=1
             order.append(course)
             for adj in graph[course]:
                 indegree[adj]-=1
                 if indegree[adj]==0:
                     q.append(adj)
-        return order if len(order)==n else []
+        return order if taken==n else []
