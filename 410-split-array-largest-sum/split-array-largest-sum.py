23c0fd7434baf6c3535b6e67f29bd8f86e394da8
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-15 20:29:09 +0530

    Time: 6 ms (14.73%) | Memory: 18 MB (20.59%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        low=max(nums)
+        high=sum(nums)
+        def find (maxi):
+            subarrays=1
+            summ=0
+            for num in nums:
+                if summ+num<=maxi:
+                    summ+=num
+                else:
+                    summ=num
+                    subarrays+=1
+            return subarrays
+        ans=None
+        while (low<=high):
+            mid=(low+high)//2
+            subarrays=find(mid)
+            if subarrays<=k:
+                ans=mid
+                high=mid-1
+            elif subarrays>k:
+                low=mid+1
+        return ans
