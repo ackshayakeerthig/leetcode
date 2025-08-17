6d5ca1df83f00461a08b1a1c831c92f5a6ec3c2f
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-17 13:13:50 +0530

    Time: 271 ms (65.35%) | Memory: 34.3 MB (60.75%) - LeetSync

--- 2118-maximum-earnings-from-taxi/maximum-earnings-from-taxi.py
+++ 2118-maximum-earnings-from-taxi/maximum-earnings-from-taxi.py
@@ -4,7 +4,7 @@ class Solution:
         dp=[(0,0)]
         for j in range(len(rides)):
             start,end,tip=rides[j]
-            i=bisect.bisect_right(dp,start,lo=0,hi=len(dp),key=lambda x :x[0])
+            i=bisect.bisect_right(dp,start,key=lambda x :x[0])
             earning=end-start+tip
             prev_earning=dp[i-1][1]
             next_earning=prev_earning+earning
