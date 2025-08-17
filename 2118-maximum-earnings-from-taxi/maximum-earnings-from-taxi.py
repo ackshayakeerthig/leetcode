502389e1ff278cff03453343c4bed8d33bc4b966
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-17 13:13:24 +0530

    Time: 267 ms (65.79%) | Memory: 34.3 MB (60.75%) - LeetSync

--- 2118-maximum-earnings-from-taxi/maximum-earnings-from-taxi.py
+++ 2118-maximum-earnings-from-taxi/maximum-earnings-from-taxi.py
@@ -1,12 +1,16 @@
 class Solution:
     def maxTaxiEarnings(self, n: int, rides: List[List[int]]) -> int:
-        rides.sort(key=lambda x:x[0])
-        @lru_cache(None)
-        def recurrence(index):
-            if index==len(rides):
-                return 0
-            start,end,tip=rides[index]
+        rides.sort(key=lambda x:x[1])
+        dp=[(0,0)]
+        for j in range(len(rides)):
+            start,end,tip=rides[j]
+            i=bisect.bisect_right(dp,start,lo=0,hi=len(dp),key=lambda x :x[0])
             earning=end-start+tip
-            next_index=bisect.bisect_left(rides,end,lo=index+1,key= lambda x: x[0])
-            return max(recurrence(next_index)+earning , recurrence(index+1))
-        return recurrence(0)
+            prev_earning=dp[i-1][1]
+            next_earning=prev_earning+earning
+            if next_earning > dp[-1][1]:
+                dp.append((end,next_earning))
+            else:
+                dp.append(dp[-1])
+        return dp[-1][1]
+            
