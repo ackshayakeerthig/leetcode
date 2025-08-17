264d2c7684ea5815684a58c3a2b0f68d0f3cfb94
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-17 16:41:38 +0530

    Time: 20 ms (64.57%) | Memory: 23.3 MB (44.22%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -3,11 +3,10 @@ class Solution:
         n=len(gas)
         start=0
         summ=0
-        if sum(gas)-sum(cost)<0:
+        if sum(gas)<sum(cost):
             return -1
         for i in range(n):
-            difference=gas[i]-cost[i]
-            summ+=difference
+            summ+=gas[i]-cost[i]
             if summ<0:
                 start=i+1
                 summ=0
