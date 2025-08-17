323ca97f34dedc91a5d8ccc5c4325945baf367e6
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-17 18:14:19 +0530

    Time: 10 ms (78.33%) | Memory: 18.2 MB (55.30%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,37 +1,28 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         board=[["."]*n for _ in range(n)]
+        cols=set()
+        diag=set()
+        antidiag=set()
         def nqueens(q):
             if q==n:
-                display()
+                ans.append(["".join(line) for line in board])
                 return
             for i in range(n):
                 if issafe(q,i):
                     board[q][i]="Q"
+                    cols.add(i)
+                    diag.add(q-i)
+                    antidiag.add(q+i)
                     nqueens(q+1)
                     board[q][i]="."
+                    cols.remove(i)
+                    diag.remove(q-i)
+                    antidiag.remove(q+i)
         def issafe(row,col):
-            for i in range(row):
-                if board[i][col]=="Q":
-                    return False
-            i,j=row-1,col-1
-            while (i>=0 and j>=0):
-                if board[i][j]=="Q":
-                    return False
-                i-=1
-                j-=1
-            i,j=row-1,col+1
-            while (i>=0 and j<n):
-                if board[i][j]=="Q":
-                    return False
-                i-=1
-                j+=1
+            if col in cols or row-col in diag or row+col in antidiag:
+                return False
             return True
         ans=[]
-        def display():
-            valid_board=[]
-            for line in board:
-                valid_board.append("".join(line))
-            ans.append(valid_board)
         nqueens(0)
         return ans
