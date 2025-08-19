aad63b34780cbeb97bd2a0ac5b4cdc712d0b1880
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-19 12:32:00 +0530

    Time: 2647 ms (7.48%) | Memory: 54.9 MB (23.21%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,25 +1,22 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        d=sum(nums)
-        if d%2!=0:
+        target=sum(nums)
+        if target%2==1:
             return False
-        d//=2
+        target//=2
         n=len(nums)
-        dp=[[-1 for i in range(0,d+1)] for j in range(n)]
-        dp[0][0]=1
-        for i in range(n):
-            dp[i][0]=0
-        return bool(self.knapsack(dp,nums,n,d))
-    def knapsack(self,dp,nums, n,d):
-        if d==0:
-            return 1
-        if n==0:
-            return 0
-        value=-1
-        if dp[n-1][d]==-1:
-            if nums[n-1]>d:
-                value=self.knapsack(dp,nums,n-1,d)
-            else:
-                value=self.knapsack(dp,nums,n-1,d-nums[n-1]) or self.knapsack(dp,nums,n-1,d)
-            dp[n-1][d]=value
-        return dp[n-1][d]
+        dp=[[-1]*(target+1) for _ in range(n)]
+        def find(index,target):
+            if target==0:
+                return True
+            if index==n-1:
+                return target==nums[index]
+            if dp[index][target]!=-1:
+                return dp[index][target]
+            donttake=find(index+1,target)
+            take=False
+            if nums[index]<=target:
+                take=find(index+1,target-nums[index])
+            dp[index][target]= take or donttake
+            return dp[index][target]
+        return find(0,target)
