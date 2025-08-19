487949ce4900a627ddc0b27e729769ae4a00f85b
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-19 13:55:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (65.85%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,13 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans=0
-        for i in range(32):
-            count=0
-            for num in nums:
-                if num & (1<<i):
-                    count+=1
-            if count%3==1:
-                ans|=(1<<i)
-        if ans>=2**31:
-            ans-=2**32
-        return ans
+        ones=0
+        twos=0
+        for num in nums:
+            ones=(ones^num)&(~twos)
+            twos=(twos^num)&(~ones)
+        return ones
