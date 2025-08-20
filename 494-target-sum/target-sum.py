11a2e12034695304752f7c8be4e8aa19b6fd6ecd
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-20 20:00:23 +0530

    Time: 55 ms (79.52%) | Memory: 19 MB (45.43%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -5,14 +5,24 @@ class Solution:
         if target%2==1:
             return 0
         target//=2
+        dp=[[-1]*(target+1) for _ in range(n)]
         def count(index,target):
-            if index==0:
-                if target==0 and nums[0]==0:
-                    return 2
-                return int(target==nums[index] or target==0)
+            if target<0:
+                return 0
+            if dp[index][target]!=-1:
+                return dp[index][target]
+            if index == 0:
+                if target == 0 and nums[0] == 0:
+                    dp[index][target] = 2
+                elif target == 0 or target == nums[0]:
+                    dp[index][target] = 1
+                else:
+                    dp[index][target] = 0
+                return dp[index][target]
             donttake=count(index-1,target)
             take=0
             if nums[index]<=target:
                 take=count(index-1,target-nums[index])
-            return donttake+take
+            dp[index][target]= donttake+take
+            return dp[index][target]
         return count(n-1,target)
