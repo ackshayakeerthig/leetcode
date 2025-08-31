db21c344916469152534ba358f69312b62348587
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-08-31 21:53:00 +0530

    Time: 194 ms (19.00%) | Memory: 18 MB (86.98%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        hash=[0]*26
+        l,r=0,0
+        maxlen=0
+        maxfreq=0
+        while r<len(s):
+            letter=s[r]
+            hash[ord(letter)-ord('A')]+=1
+            maxfreq=max(maxfreq,hash[ord(letter)-ord('A')])
+            while r-l+1 - maxfreq>k:
+                hash[ord(s[l])-ord('A')]-=1
+                maxfreq=max(hash)
+                l+=1
+                
+            if r-l+1 - maxfreq <=k:
+                maxlen=max(maxlen,r-l+1)
+            r+=1
+        return maxlen
+            
