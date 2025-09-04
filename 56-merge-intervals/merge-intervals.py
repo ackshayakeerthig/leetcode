00e7452a8bc6d21e6c195ed664ed207956301912
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-09-05 00:09:32 +0530

    Time: 9 ms (69.73%) | Memory: 17.2 MB (7.85%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -6,21 +6,13 @@ class Solution(object):
         """
         intervals.sort(key=lambda x: x[0])
         result=[]
-        # i_start=intervals[0][0]
-        # i_end=intervals[0][1]
         result.append(intervals[0])
         for start,end in intervals:
             i_start=result[-1][0]
             i_end=result[-1][1]
             if start > i_end:
-                # result.append([i_start,i_end])
                 result.append([start,end])
-                # i_start=start
-                # i_end=end
             else:
                 result[-1]=[i_start,max(i_end, end)]
-            #     i_start=min(i_start,start)
-            #     i_end=max(i_end,end)
-        # result.append([i_start,i_end])
         return result
         
