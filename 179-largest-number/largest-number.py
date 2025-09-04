624cb2c0fbafc70791ed634c44fb42b134e1449a
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-09-05 00:30:28 +0530

    Time: 4 ms (66.43%) | Memory: 12.5 MB (46.70%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -5,14 +5,15 @@ class Solution(object):
         :rtype: str
         """
         nums = list(map(str, nums))
-        def compare(a,b):
-            if a+b>b+a:
-                return -1
-            elif a+b<b+a:
-                return 1
-            else:
-                return 0
-        nums.sort(key=cmp_to_key(compare))
+        # def compare(a,b):
+        #     if a+b>b+a:
+        #         return -1
+        #     elif a+b<b+a:
+        #         return 1
+        #     else:
+        #         return 0
+        # nums.sort(key=cmp_to_key(compare))
+        nums.sort(key=lambda x: x*10, reverse=True)
         result="".join(nums)
         if result[0]=='0':
             return "0"
