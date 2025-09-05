3e14634e4150f969869b9e08f62029fb8fadb65c
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-09-05 11:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (17.10%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -6,19 +6,24 @@ class Solution(object):
         :rtype: List[int]
         """
         n=len(nums2)
+        d={}
         nextg=[0]*n
         stack=[nums2[n-1]]
-        nextg[n-1]=-1
+        # nextg[n-1]=-1
+        d[nums2[n-1]]=-1
         for i in range(n-2,-1,-1):
             while stack and stack[-1]<nums2[i]:
                 stack.pop()
-            nextg[i]=stack[-1] if stack else -1
+            # nextg[i]=stack[-1] if stack else -1
+            d[nums2[i]]=stack[-1] if stack else -1
             stack.append(nums2[i])
         result=[]
         for num in nums1:
-            for i in range(n):
-                if num==nums2[i]:
-                    result.append(nextg[i])
+            # for i in range(n):
+            #     if num==nums2[i]:
+            #         result.append(nextg[i])
+            if num in d:
+                result.append(d[num])
         return result
 
 
