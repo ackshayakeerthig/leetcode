dc331e7a41047ab8b88235d437a714b5a2b2d30f
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-09-11 15:02:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (53.01%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -5,16 +5,16 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        # n1=int(num1)
-        # n2=int(num2)
-        # return str(n1*n2)
-        n1=0
-        n2=0
-        for char in num1:
-            n1=n1*10+(ord(char)-ord("0"))
-        for char in num2:
-            n2=n2*10+ord(char)-ord("0")
-        result=n1*n2
+        n1=int(num1)
+        n2=int(num2)
         return str(n1*n2)
+        # n1=0
+        # n2=0
+        # for char in num1:
+        #     n1=n1*10+(ord(char)-ord("0"))
+        # for char in num2:
+        #     n2=n2*10+ord(char)-ord("0")
+        # result=n1*n2
+        # return str(n1*n2)
 
         
