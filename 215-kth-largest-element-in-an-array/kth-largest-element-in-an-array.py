2b4c2130bce36ee1e47a2361bff93d623a140d6f
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-09-14 10:24:12 +0530

    Time: 483 ms (50.25%) | Memory: 20.5 MB (20.57%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -5,6 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        nums=[-n for n in nums]
         heapq.heapify(nums)
-        return heapq.nlargest(k,nums)[-1] 
+        ans=0
+        i=0
+        while nums and i<k:
+            ans=-heapq.heappop(nums)
+            i+=1
+        return ans
+        
         
