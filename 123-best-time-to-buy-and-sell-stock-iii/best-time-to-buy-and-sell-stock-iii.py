0b9027d2582433cb444a13a9c2825d923760025e
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-09-26 14:36:27 +0530

    Time: 723 ms (20.89%) | Memory: 21.2 MB (46.29%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -5,20 +5,21 @@ class Solution(object):
         :rtype: int
         """
         n=len(prices)
-        dp=[[[-1]*3 for _ in range(2)] for _ in range(n+1)]
-        for index in range(n):
-            for buy in range(2):
-                dp[index][buy][0]=0
+        cur=[[0]*3 for _ in range(2)] 
+        after=[[0]*3 for _ in range(2)] 
+        # for buy in range(2):
+        #         after[buy][0]=0
         
-        for buy in range(2):
-            for cap in range(3):
-                dp[n][buy][cap]=0
+        # for buy in range(2):
+        #     for cap in range(3):
+        #         after[buy][cap]=0
         for index in range(n-1,-1,-1):
             for buy in range(2):
                 for cap in range(1,3):
                     if buy==1:
-                        profit=max(dp[index+1][0][cap]-prices[index],dp[index+1][1][cap])
+                        profit=max(after[0][cap]-prices[index],after[1][cap])
                     else:
-                        profit=max(dp[index+1][1][cap-1]+prices[index],dp[index+1][0][cap])
-                    dp[index][buy][cap]=profit
-        return dp[0][1][2]
+                        profit=max(after[1][cap-1]+prices[index],after[0][cap])
+                    cur[buy][cap]=profit
+            cur,after=after,cur
+        return after[1][2]
