c7d15b1f10bffc57cc515271abf0dbd982903814
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2025-11-29 12:47:00 +0530

    Time: 3 ms (11.36%) | Memory: 18 MB (17.56%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,29 +1,16 @@
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         n=len(nums)
-        dp=[0]*n
-        if n<=0:
-            return 0
-        dp[0]=nums[0]
-        if n>=2:
-            dp[1]=max(nums[0],nums[1])
-        else:
-            return dp[0]
+        dp1=[0 for _ in range(n)]
+        dp2=[0 for _ in range(n)]
+        dp2[0]=nums[0]
+        if n>1:
+            dp2[1]=max(dp2[0],nums[1]) 
+            dp1[1]=nums[1]
+        if n>2:
+            dp1[2]=max(nums[1],nums[2])
+        for i in range(3,n):
+            dp1[i]=max(dp1[i-2]+nums[i], dp1[i-1])
         for i in range(2,n-1):
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1])
-        dp2=[0]*n
-        if n<=0:
-            return 0
-        dp2[n-1]=nums[n-1]
-        if n>=2:
-            dp2[n-2]=max(nums[n-1],nums[n-2])
-        else:
-            return max(dp2[n-1],dp[0])
-        for i in range(n-3,0,-1):
-            dp2[i]=max(nums[i]+dp2[i+2],dp2[i+1])
-        return max(dp[n-2],dp2[1])
-        
+            dp2[i]=max(dp2[i-2]+nums[i], dp2[i-1])
+        return max(dp1[n-1],dp2[n-2])
