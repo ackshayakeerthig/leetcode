66b2005470e12c91d2c15fb6ca123422abec0859
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2026-06-29 00:15:27 +0530

    Time: 786 ms (21.77%) | Memory: 49.9 MB (93.55%) - LeetSync

---
+++ 1860-find-kth-largest-xor-coordinate-value/find-kth-largest-xor-coordinate-value.py
@@ -0,0 +1,23 @@
+class Solution:
+    def kthLargestValue(self, matrix: List[List[int]], k: int) -> int:
+        h=[]
+        m=len(matrix)
+        n=len(matrix[0])
+        if m<1 or n<1:
+            return 0
+        for i in range(m):
+            for j in range(n):
+                if i==0 and j==0:
+                    pass
+                elif i==0:
+                    matrix[i][j]=matrix[i][j]^matrix[i][j-1]
+                elif j==0:
+                    matrix[i][j]=matrix[i][j]^matrix[i-1][j]
+
+                else:
+                    matrix[i][j]=matrix[i][j]^matrix[i][j-1]^matrix[i-1][j]^matrix[i-1][j-1]
+
+                heapq.heappush(h,matrix[i][j])
+                if len(h)>k:
+                    heapq.heappop(h)
+        return h[-k]
