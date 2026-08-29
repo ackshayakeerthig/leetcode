5cc1c9bc2aefe948f286f990fc0adb14dfba7115
ackshayakeerthig <151990722+ackshayakeerthig@users.noreply.github.com>
2026-08-29 20:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.29%) - LeetSync

---
+++ 263-ugly-number/ugly-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n==0:
+            return False
+        while n%(2*3*5)==0:
+            n//=(2*3*5)
+        while n%(2*3)==0:
+            n//=(2*3)
+        while n%(2*5)==0:
+            n//=(2*5)
+        while n%(3*5)==0:
+            n//=(3*5)
+        while n%2==0:
+            n//=2
+        while n%3==0:
+            n//=3
+        while n%5==0:
+            n//=5
+        return True if n==1 else False
